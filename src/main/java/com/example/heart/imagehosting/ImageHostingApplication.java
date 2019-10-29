9e47d460409e1b18be1ce5c26dc49757e03ccf2d
heart14 <lwf14@qq.com>
2019-10-29 14:39:10 +0800

    project init

---
+++ src/main/java/com/example/heart/imagehosting/ImageHostingApplication.java
@@ -0,0 +1,13 @@
+package com.example.heart.imagehosting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageHostingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageHostingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/heart/imagehosting/ImageHostingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.heart.imagehosting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageHostingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
